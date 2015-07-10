ff00432fcbbd72a3e074fa4bfc8d5a3291966b95
xueqian <xueqian0321@outlook.com>
2015-07-10 14:51:31 +0800

    step 4--modify UserDao

--- core/src/main/java/com/tw/core/dao/UserDao.java
+++ core/src/main/java/com/tw/core/dao/UserDao.java
@@ -3,85 +3,59 @@ package com.tw.core.dao;
 import com.tw.core.entity.User;
 import com.tw.core.util.HibernateUtil;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
 import java.util.List;
 
 public class UserDao {
 
+
     public List<User> getUsers(){
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
-        Session session = sessionFactory.openSession();
-
-        session.beginTransaction();
+        Session session = HibernateUtil.getSessionFactory().openSession();
 
         List<User> userList = session.createQuery("from User").list();
 
-        session.getTransaction().commit();
-        sessionFactory.close();
-
+        session.close();
         return userList;
     }
 
     public void addUser(User user){
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
-        Session session = sessionFactory.openSession();
+        Session session = HibernateUtil.getSessionFactory().openSession();
 
         session.beginTransaction();
-
         session.save(user);
-
         session.getTransaction().commit();
+
         session.close();
-        sessionFactory.close();
     }
 
     public void deleteUser(int id){
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
-        Session session = sessionFactory.openSession();
+        Session session = HibernateUtil.getSessionFactory().openSession();
 
         session.beginTransaction();
-
         User user = (User)session.load(User.class,id);
-
         session.delete(user);
-
         session.getTransaction().commit();
 
-        sessionFactory.close();
+        session.close();
     }
 
     public User getUserById(int id){
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
-        Session session = sessionFactory.openSession();
-
-        session.beginTransaction();
+        Session session = HibernateUtil.getSessionFactory().openSession();
 
         User user = (User)session.get(User.class,id);
 
-        session.getTransaction().commit();
-
-        sessionFactory.close();
-
+        session.close();
         return user;
     }
 
     public void updateUser(User user){
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
-        Session session = sessionFactory.openSession();
+        Session session = HibernateUtil.getSessionFactory().openSession();
 
         session.beginTransaction();
-
         session.update(user);
-
         session.getTransaction().commit();
 
-        sessionFactory.close();
+        session.close();
     }
 }
 
