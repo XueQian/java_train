f783072a98706175b192984ffbc8f43fe9637d17
xueqian <xueqian0321@outlook.com>
2015-07-10 15:40:17 +0800

    step 4--modify getCurrentSession

--- core/src/main/java/com/tw/core/dao/UserDao.java
+++ core/src/main/java/com/tw/core/dao/UserDao.java
@@ -10,52 +10,48 @@ public class UserDao {
 
 
     public List<User> getUsers(){
-        Session session = HibernateUtil.getSessionFactory().openSession();
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
 
         List<User> userList = session.createQuery("from User").list();
+        session.getTransaction().commit();
 
-        session.close();
         return userList;
     }
 
     public void addUser(User user){
-        Session session = HibernateUtil.getSessionFactory().openSession();
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 
         session.beginTransaction();
         session.save(user);
         session.getTransaction().commit();
-
-        session.close();
     }
 
     public void deleteUser(int id){
-        Session session = HibernateUtil.getSessionFactory().openSession();
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 
         session.beginTransaction();
         User user = (User)session.load(User.class,id);
         session.delete(user);
         session.getTransaction().commit();
-
-        session.close();
     }
 
     public User getUserById(int id){
-        Session session = HibernateUtil.getSessionFactory().openSession();
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 
+        session.beginTransaction();
         User user = (User)session.get(User.class,id);
+        session.getTransaction().commit();
 
-        session.close();
         return user;
     }
 
     public void updateUser(User user){
-        Session session = HibernateUtil.getSessionFactory().openSession();
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 
         session.beginTransaction();
         session.update(user);
         session.getTransaction().commit();
-
-        session.close();
     }
 }
 
