d2c5026aa97ada9d9b57c5e7bfda847ca111c762
xueqian <xueqian0321@outlook.com>
2015-07-13 15:07:43 +0800

    step 5--finish MD5Util.java

---
+++ core/src/main/java/com/tw/core/util/MD5Util.java
@@ -0,0 +1,21 @@
+package com.tw.core.util;
+
+import java.security.MessageDigest;
+
+/**
+ * Created by qxue on 7/13/15.
+ */
+public class MD5Util {
+    public String getMD5(String string) throws Exception {
+
+        MessageDigest md5 = MessageDigest.getInstance("md5");
+        byte[] cipherData = md5.digest(string.getBytes());
+        StringBuilder builder = new StringBuilder();
+
+        for (byte cipher : cipherData) {
+            String toHexStr = Integer.toHexString(cipher & 0xff);
+            builder.append(toHexStr.length() == 1 ? "0" + toHexStr : toHexStr);
+        }
+        return builder.toString();
+    }
+}
