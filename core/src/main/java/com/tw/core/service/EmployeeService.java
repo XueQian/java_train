8448ac1d1f1f292d2d90366b21ed93c20c45632a
xueqian <xueqian0321@outlook.com>
2015-07-16 16:23:48 +0800

    step 6--finish register at service/EmployeeService

--- core/src/main/java/com/tw/core/service/EmployeeService.java
+++ core/src/main/java/com/tw/core/service/EmployeeService.java
@@ -1,7 +1,26 @@
 package com.tw.core.service;
 
+import com.tw.core.dao.EmployeeDao;
+import com.tw.core.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by qxue on 7/16/15.
  */
+@Service
 public class EmployeeService {
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+    public void addEmployee(Employee employee) {
+        employeeDao.addEmployee(employee);
+    }
+
+    public List<Employee> getEmployeeByName(String name) {
+        return employeeDao.getEmployeeByName(name);
+    }
 }
