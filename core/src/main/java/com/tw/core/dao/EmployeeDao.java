b98f230c6d50cbcf7f5e11982734a9fe2f7794f9
xueqian <xueqian0321@outlook.com>
2015-07-16 16:22:48 +0800

    step 6--finish register at dao/EmployeeDao

--- core/src/main/java/com/tw/core/dao/EmployeeDao.java
+++ core/src/main/java/com/tw/core/dao/EmployeeDao.java
@@ -2,8 +2,11 @@ package com.tw.core.dao;
 
 import com.tw.core.entity.Employee;
 import com.tw.core.util.HibernateUtil;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by qxue on 7/16/15.
@@ -11,12 +14,25 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class EmployeeDao {
 
-    public void addEmployee(Employee employee){
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+    public void addEmployee(Employee employee) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
 
         session.beginTransaction();
         session.save(employee);
         session.getTransaction().commit();
+
+        session.close();
     }
 
+    public List<Employee> getEmployeeByName(String name) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+//        session.beginTransaction();
+        Query query = session.createQuery("from Employee where user_name=:name");
+        List employeeList = query.setString("name", name).list();
+//        session.getTransaction().commit();
+        session.close();
+
+        return employeeList;
+    }
 }
