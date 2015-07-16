4b5789288ccb6f0c37dc7357b93ce40e63298c8d
xueqian <xueqian0321@outlook.com>
2015-07-16 13:58:18 +0800

    step 6--finish entity/Customer

---
+++ core/src/main/java/com/tw/core/entity/Customer.java
@@ -0,0 +1,46 @@
+package com.tw.core.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by qxue on 7/16/15.
+ */
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "employee_id")
+    private String employeeId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+}
