01dfd10883dd12d0a3b7c2f7709c5896f1d01040
xueqian <xueqian0321@outlook.com>
2015-07-16 13:48:16 +0800

    step 6--finish entity/Employee

---
+++ core/src/main/java/com/tw/core/entity/Employee.java
@@ -0,0 +1,46 @@
+package com.tw.core.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by qxue on 7/16/15.
+ */
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "role")
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
