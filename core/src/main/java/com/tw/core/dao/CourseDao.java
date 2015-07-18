a3375f63dfd1f994f9cecff1543f3390dd4c11c7
xueqian <xueqian0321@outlook.com>
2015-07-18 11:03:31 +0800

    step 6--finish getCourses course name at dao/Course

--- core/src/main/java/com/tw/core/dao/CourseDao.java
+++ core/src/main/java/com/tw/core/dao/CourseDao.java
@@ -1,7 +1,38 @@
 package com.tw.core.dao;
 
+import com.tw.core.entity.Course;
+import com.tw.core.util.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.transform.RootEntityResultTransformer;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
  * Created by qxue on 7/16/15.
  */
+@Repository
 public class CourseDao {
+
+    public List<Course> getCourses(){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+
+        List<Course> courseList = session.createQuery("from Course").list();
+        session.getTransaction().commit();
+
+        return courseList;
+    }
+
+    public List<Course> getCourseByName(String name) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+        session.beginTransaction();
+        Query query = session.createQuery("from Course where name=:name");
+        List courseList = query.setString("name", name).list();
+        session.getTransaction().commit();
+
+        return courseList;
+    }
 }
