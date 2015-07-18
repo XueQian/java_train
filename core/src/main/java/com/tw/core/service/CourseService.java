012d523f7c44a9ba10ee8cd5f47bc7bd508bef7d
xueqian <xueqian0321@outlook.com>
2015-07-18 11:07:57 +0800

    step 6--finish getCourses at service/Course

--- core/src/main/java/com/tw/core/service/CourseService.java
+++ core/src/main/java/com/tw/core/service/CourseService.java
@@ -1,7 +1,22 @@
 package com.tw.core.service;
 
+import com.tw.core.dao.CourseDao;
+import com.tw.core.entity.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by qxue on 7/17/15.
  */
+@Service
 public class CourseService {
+
+    @Autowired
+    private CourseDao courseDao;
+
+    public List<Course> getCourses(){
+        return courseDao.getCourses();
+    }
 }
