63e389689d47d417c735671ea39b35d2f1b9990a
xueqian <xueqian0321@outlook.com>
2015-07-19 23:39:58 +0800

    step 6--finish add private coach at service/course

--- core/src/main/java/com/tw/core/service/CustomerService.java
+++ core/src/main/java/com/tw/core/service/CustomerService.java
@@ -1,7 +1,20 @@
 package com.tw.core.service;
 
+import com.tw.core.dao.CustomerDao;
+import com.tw.core.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by qxue on 7/19/15.
  */
+@Service
 public class CustomerService {
+
+    @Autowired
+    CustomerDao customerDao;
+
+    public void addCustomer(Customer customer){
+        customerDao.addCustomer(customer);
+    }
 }
