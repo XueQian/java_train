a9b7e170f17bf25ec3432a09b01e021d0b17e091
xueqian <xueqian0321@outlook.com>
2015-07-19 23:37:21 +0800

    step 6--add file to add private coach

---
+++ core/src/main/java/com/tw/core/dao/CustomerDao.java
@@ -0,0 +1,7 @@
+package com.tw.core.dao;
+
+/**
+ * Created by qxue on 7/19/15.
+ */
+public class CustomerDao {
+}
---
+++ core/src/main/java/com/tw/core/service/CustomerService.java
@@ -0,0 +1,7 @@
+package com.tw.core.service;
+
+/**
+ * Created by qxue on 7/19/15.
+ */
+public class CustomerService {
+}
