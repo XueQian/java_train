329baa530a98ca5af7b9f6339bdf4e47a7b378c8
xueqian <xueqian0321@outlook.com>
2015-07-20 12:02:30 +0800

    step 6--add getCustomerByName at dao/customer

--- core/src/main/java/com/tw/core/dao/CustomerDao.java
+++ core/src/main/java/com/tw/core/dao/CustomerDao.java
@@ -1,7 +1,9 @@
 package com.tw.core.dao;
 
 import com.tw.core.entity.Customer;
+import com.tw.core.entity.Employee;
 import com.tw.core.util.HibernateUtil;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
@@ -22,5 +24,23 @@ public class CustomerDao {
             session.getTransaction().rollback();
             throw e;
         }
+    }
+
+    public Customer getCustomerByName(String name){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Customer customer;
+
+        try {
+            session.beginTransaction();
+            Query query = session.createQuery("from Customer where name=:name");
+            query.setString("name", name);
+            customer = (Customer) query.uniqueResult();
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            session.getTransaction().rollback();
+            throw e;
+        }
+        return customer;
+
     }
 }
