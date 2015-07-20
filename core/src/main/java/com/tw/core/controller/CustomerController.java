c82ebf2c378989958cbe2715fc41d9734760fa10
xueqian <xueqian0321@outlook.com>
2015-07-21 00:15:43 +0800

    step 6--add getCustomers at controller/customer

--- core/src/main/java/com/tw/core/controller/CustomerController.java
+++ core/src/main/java/com/tw/core/controller/CustomerController.java
@@ -1,7 +1,33 @@
 package com.tw.core.controller;
 
+import com.tw.core.entity.Customer;
+import com.tw.core.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
 /**
  * Created by qxue on 7/21/15.
  */
+@RestController
+@RequestMapping("/")
 public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @RequestMapping(value = "/customers", method = RequestMethod.GET)
+    public ModelAndView getEmployees() {
+
+        List<Customer> customerList = customerService.getCustomers();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("customers");
+        modelAndView.addObject("customers", customerList);
+        return modelAndView;
+    }
 }
