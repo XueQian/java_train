797df8d2d905fcfacf3ce0c79d22aa025fd08991
xueqian <xueqian0321@outlook.com>
2015-07-20 22:51:03 +0800

    step 6--finish getEmployees at controller/employee

--- core/src/main/java/com/tw/core/controller/EmployeeController.java
+++ core/src/main/java/com/tw/core/controller/EmployeeController.java
@@ -1,7 +1,33 @@
 package com.tw.core.controller;
 
+import com.tw.core.entity.Employee;
+import com.tw.core.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
 /**
  * Created by qxue on 7/20/15.
  */
+@RestController
+@RequestMapping("/")
 public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @RequestMapping(value = "/employees", method = RequestMethod.GET)
+    public ModelAndView getEmployees() {
+
+        List<Employee> employeeList = employeeService.getEmployees();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("employees");
+        modelAndView.addObject("employees", employeeList);
+        return modelAndView;
+    }
 }
