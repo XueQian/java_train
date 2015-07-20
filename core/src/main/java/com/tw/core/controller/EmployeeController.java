6b79b9f25731b68fcac7487c8d7aa059a9ab1f24
xueqian <xueqian0321@outlook.com>
2015-07-20 23:17:15 +0800

    add getUpdateEmployeePage at controller/employee

--- core/src/main/java/com/tw/core/controller/EmployeeController.java
+++ core/src/main/java/com/tw/core/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.tw.core.controller;
 import com.tw.core.entity.Employee;
 import com.tw.core.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,17 @@ public class EmployeeController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("employees");
         modelAndView.addObject("employees", employeeList);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/employees/modification/{id}", method = RequestMethod.GET)
+    public ModelAndView getUpdateEmployeePage(@PathVariable int id) {
+
+        Employee employee = employeeService.getEmployeeById(id);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("updateEmployee");
+        modelAndView.addObject("employee", employee);
         return modelAndView;
     }
 }
