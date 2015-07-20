f0c34328ec7f69fe5d7ca53a6f05801069ccaaca
xueqian <xueqian0321@outlook.com>
2015-07-20 22:00:34 +0800

    step 6--finish update user at entity/user

--- core/src/main/java/com/tw/core/entity/User.java
+++ core/src/main/java/com/tw/core/entity/User.java
@@ -20,6 +20,13 @@ public class User {
         this.employee = employee;
     }
 
+    public User(int id, String name, String password,Employee employee) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.employee = employee;
+    }
+
     public User() {
     }
 
