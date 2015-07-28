64ab38adcd44ac4e87c11bef6a9b6ca731b16a13
xueqian <xueqian0321@outlook.com>
2015-07-28 23:30:23 +0800

    getCourses by angular at scripts/app

--- web/src/main/webapp/scripts/app.js
+++ web/src/main/webapp/scripts/app.js
@@ -1,3 +1,18 @@
-/**
- * Created by qxue on 7/27/15.
- */
+'use strict';
+
+angular
+    .module('gymSystem', [
+        'ngRoute'
+    ])
+    .config(function($routeProvider) {
+
+        $routeProvider
+            .when('/courses', {
+                templateUrl: 'views/course.html',
+                controller: 'courseController'
+            })
+            .otherwise({
+                redirectTo: '/1111111'
+            });
+
+    });
