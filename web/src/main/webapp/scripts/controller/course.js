5f5d11ff1e7af811a6b37259a3c91d9f09bef584
xueqian <xueqian0321@outlook.com>
2015-07-28 23:31:27 +0800

    getCourses by angular at controller/course

--- web/src/main/webapp/scripts/controller/course.js
+++ web/src/main/webapp/scripts/controller/course.js
@@ -1,3 +1,23 @@
-/**
- * Created by qxue on 7/28/15.
- */
+'use strict';
+
+var module = angular.module('gymSystem').controller('courseController', function($scope, CourserService) {
+
+    CourserService.getCourses(function(data) {
+
+        $scope.courses = data;
+    });
+});
+
+module.factory('CourserService', function($http) {
+
+    return {
+        getCourses: function(callback) {
+
+            $http.get('api/courses').
+                success(function(data) {
+
+                    callback(data);
+                });
+        }
+    }
+});
