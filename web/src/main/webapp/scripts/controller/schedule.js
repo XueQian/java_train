4c3a62216ca1605eab174970a7c9301106023c03
xueqian <xueqian0321@outlook.com>
2015-07-30 00:43:52 +0800

    add schedule by angular at scripts/controller/schedule

--- web/src/main/webapp/scripts/controller/schedule.js
+++ web/src/main/webapp/scripts/controller/schedule.js
@@ -1,10 +1,40 @@
 'use strict';
 
-angular.module('gymSystem').controller('scheduleController', function($scope, ScheduleService) {
+angular.module('gymSystem').controller('scheduleController', function($scope, ScheduleService,CourseService,EmployeeService) {
 
     getSchedules();
 
+    $scope.showAddSchedule = function() {
+
+        $scope.addFlag = false;
+
+        CourseService.getCourses(function(data) {
+
+            $scope.courses = data;
+        });
+
+        EmployeeService.getEmployees(function(data){
+
+            $scope.employees = data;
+        })
+    };
+
+    $scope.addSchedule = function(){
+
+       ScheduleService.addSchedule($scope.employeeId,$scope.courseId,$scope.time,function(){
+
+           getSchedules();
+       });
+    };
+
+    $scope.hideAddSchedule = function() {
+
+        $scope.addFlag = true;
+    };
+
     function getSchedules() {
+
+        $scope.addFlag = true;
 
         ScheduleService.getSchedules(function(data) {
 
