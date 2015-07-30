2f0a54e1a9e7ee387cea61ffaa2157b7f1fffa9f
xueqian <xueqian0321@outlook.com>
2015-07-30 18:09:26 +0800

    add private coach at scripts/service/schedule

--- web/src/main/webapp/scripts/service/scheduleService.js
+++ web/src/main/webapp/scripts/service/scheduleService.js
@@ -29,6 +29,31 @@ angular.module('gymSystem').service('ScheduleService', function($http) {
             alert("该教练时间冲突，请重新操作！");
         });
     };
+
+    this.addPrivateCoach = function(customerId, employeeId, time, callback) {
+
+        $http({
+            method: 'POST',
+            url: 'api/schedules/private',
+            params: {
+                customerId: customerId,
+                employeeId: employeeId,
+                time: time.toISOString().substring(0, 10)
+            }
+        }).success(function() {
+
+            callback();
+        }).error(function(data, status) {
+
+            if(status === 409) {
+
+                alert("该教练时间冲突，请重新操作");
+            } else if(status === 417) {
+
+                alert('该顾客已存在私教，请重新操作');
+            }
+        });
+    };
 });
 
 
