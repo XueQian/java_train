611c6e2369ba9ef06a5d3410e3d3b373201a7921
xueqian <xueqian0321@outlook.com>
2015-07-30 23:38:28 +0800

    get users by angular at scripts/service/user

--- web/src/main/webapp/scripts/service/userService.js
+++ web/src/main/webapp/scripts/service/userService.js
@@ -1,3 +1,13 @@
-/**
- * Created by qxue on 7/30/15.
- */
+'use strict';
+
+angular.module('gymSystem').service('UserService', function($http) {
+
+    this.getUsers = function(callback) {
+
+        $http.get('api/users').
+            success(function(data) {
+
+                callback(data);
+            });
+    };
+});
