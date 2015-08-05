813d35f8178790ab57f6cb6c392f4d0a6da83e83
xueqian <xueqian0321@outlook.com>
2015-08-05 14:22:10 +0800

    test --finish dao/getEmployees

---
+++ core/src/test/java/com/tw/core/dao/EmployeeDaoTest.java
@@ -0,0 +1,43 @@
+package com.tw.core.dao;
+
+import com.tw.core.entity.Employee;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+//@ActiveProfiles(profiles = "test",inheritProfiles = true)
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath*:/applicationContext.xml"})
+@Rollback(true)
+public class EmployeeDaoTest {
+
+    @BeforeClass
+    public static void setSystemProperty() {
+        System.setProperty("spring.profiles.active", "test");
+    }
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @Test
+    public void testGetEmployees() {
+
+        List<Employee> employees = employeeDao.getEmployees();
+
+        Assert.assertEquals(employees.size(), 1);
+
+        Assert.assertEquals(employees.get(0).getName(),"测试");
+
+    }
+}
--- core/src/test/java/com/tw/core/dao/daoTest.java
+++
@@ -1,7 +0,0 @@
-package com.tw.core.dao;
-
-/**
- * Created by qxue on 8/4/15.
- */
-public class daoTest {
-}
