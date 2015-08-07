94c79225df63cfda456d42bb5161c0daeaf79718
xueqian <xueqian0321@outlook.com>
2015-08-07 14:48:25 +0800

    e2e-finish log4js

---
+++ web/src/test/e2e/utils/logger.js
@@ -0,0 +1,14 @@
+var log4js = require('log4js');
+
+log4js.configure({
+    appenders: [
+        { type: 'console' },
+        { type: 'file', filename: 'reports/e2e-test.txt'}
+    ]
+});
+
+var logger = log4js.getLogger('E2E-test');
+//TRACE, DEBUG, INFO, WARN, ERROR, FATAL
+logger.setLevel('TRACE');
+
+module.exports = logger;
