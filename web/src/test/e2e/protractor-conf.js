9d3998ef79e0796b919a1f393fce73fcb0ecaec7
xueqian <xueqian0321@outlook.com>
2015-08-07 14:45:47 +0800

    config .gitignore

---
+++ web/src/test/e2e/protractor-conf.js
@@ -0,0 +1,49 @@
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec.js'],
+    multiCapabilities: [{
+        browserName: 'firefox'
+    }, {
+        browserName: 'chrome'
+    }]
+     capabilities: {
+    //        'browserName': 'chrome' //'firefox'
+    //    },
+}
+
+
+//exports.config = {
+//    allScriptsTimeout: 30000,
+//
+//    specs: [
+//        'spec.js'
+//    ],
+//
+//    capabilities: {
+//        'browserName': 'chrome' //'firefox'
+//    },
+//
+//    chromeOnly: false,
+//
+//    baseUrl: 'http://localhost:8000/',
+//
+//    framework: 'jasmine',
+//    //
+//    //onPrepare: function () {
+//    //    'use strict';
+//    //
+//    //    browser.addMockModule('mock', function () {
+//    //        angular.module('mock', [])
+//    //            .run(function ($animate) {
+//    //                $animate.enabled(false);
+//    //            });
+//    //    });
+//    //},
+//
+//    debug: false,
+//
+//    jasmineNodeOpts: {
+//        showColors: true,
+//        defaultTimeoutInterval: 30000
+//    }
+//};
